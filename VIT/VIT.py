35ea3f5804d704d5dff364566ce0571ddf541e41
Suizhi Huang <759833174@qq.com>
2024-06-13 21:17:18 +0800

    feat: Add initial implementation of VisionTransformer model

---
+++ VIT/VIT.py
@@ -0,0 +1,29 @@
+import logging
+import math
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+from timm.models.layers import DropPath, StdConv2dSame, to_2tuple, trunc_normal_
+
+
+class PatchEmbed(nn.Module):
+    pass
+
+
+class Attention(nn.Module):
+    pass
+
+
+class Block(nn.Module):
+    pass
+
+
+class VisionTransformer(nn.Module):
+    pass
+
+
+class FFN(nn.Module):
+    pass
